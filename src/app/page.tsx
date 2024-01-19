fd3b5a75316723073d974bc840751d7a58ec3b22
mon7792 <monish7792@gmail.com>
2024-01-19 21:07:16 +0530

    added shad cn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { UserButton } from "@clerk/nextjs";
- 
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="h-screen">
       <h1>Hello</h1>
-      <UserButton afterSignOutUrl="/"/>
+      <Button variant="outline">Button</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
-  )
+  );
 }
