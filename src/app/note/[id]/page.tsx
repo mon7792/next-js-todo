05838659fc9eb4e30f58bc9225631f80adaca421
mon7792 <monish7792@gmail.com>
2024-01-23 19:55:04 +0530

    updated the details page

--- src/app/note/[id]/page.tsx
+++ src/app/note/[id]/page.tsx
@@ -2,7 +2,8 @@ import NoteDetailSection from "@/components/section/note/detail";
 import { getCachedNote } from "@/driver/cache/note";
 import { Note } from "@/types/note";
 
-export default async function Page() {
-  const note: Note = await getCachedNote("uid-123");
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id as string;
+  const note: Note = await getCachedNote(id);
   return <NoteDetailSection note={note} />;
 }
